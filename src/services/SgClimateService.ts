ddc43012bbc7efeba25ea5029c64c6e2fcf1bce3
ThibautMg <139113604+ThibautMg@users.noreply.github.com>
2023-11-10 07:34:55 +0530

    Update COP + December SgClimateService.ts

--- src/services/SgClimateService.ts
+++ src/services/SgClimateService.ts
@@ -1,38 +1,73 @@
 const participantSessionList: Session[] = [
    {
-    id: 2,
-    date: "06 November 2023 (Monday evening)",
-    sessionType: "Climate Fresk",
-    host: "Babilou Family - Alexandra Rd",
-    url: "https://association.climatefresk.org/training_sessions/a4ba3683-bf51-463e-81f1-62062a4b0be1/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
-  },
-   {
-    id: 3,
-    date: "07 November 2023 (Tuesday evening)",
-    sessionType: "Biodiversity Collage",
-    host: "L'Oréal Offices - One Raffles Quay",
-    url: "https://www.eventbrite.com/e/biodiversity-collage-loreal-singapore-tickets-658117395847?aff=oddtdtcreator",
-  },
-   {
-    id: 5,
-    date: "18 November 2023",
-    sessionType: "COP28 - Climate Fresk for Kids",
+    id: 1,
+    date: "18 November 2023 - Saturday morning",
+    sessionType: "COP28 - Climate Fresk for Adults & Kids",
     host: "Accueil Singapour",
     url: "https://association.climatefresk.org/training_sessions/1d806844-86f3-451e-9de3-6fcfe113b6e9/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
   },
   {
-    id: 6,
-    date: "18 November 2023",
-    sessionType: "COP28 - Climate Fresk, 2 Tonnes, Climate Fresk for kids, Biodiversity Collage",
-    host: "CIVIC",
+    id: 2,
+    date: "18 November 2023 - Saturday morning",
+    sessionType: "COP28 Climate Marathon - Climate Fresk for Adults & Kids, 2 Tonnes, Biodiversity Collage, Digital Collage",
+    host: "Ismaili CIVIC",
     url: "https://www.eventbrite.sg/e/climate-marathon-to-celebrate-cop28-tickets-749866038507?aff=oddtdtcreator",
   },
   {
-    id: 7,
-    date: "18 November 2023",
-    sessionType: "COP28 - Climate Fresk, 2 Tonnes, Climate Fresk for kids, Biodiversity Collage",
+    id: 3,
+    date: "18 November 2023 - Saturday afternoon",
+    sessionType: "COP28 Climate Marathon - Climate Fresk for Adults & Kids, 2 Tonnes, Biodiversity Collage, Digital Collage",
     host: "Palo IT",
     url: "https://www.eventbrite.sg/e/climate-marathon-to-celebrate-cop28-tickets-749811274707?aff=oddtdtcreator",
+  }
+   {
+    id: 4,
+    date: "25 November 2023 - Saturday afternoon",
+    sessionType: "COP28 Climate Marathon - Climate Fresk for Adults & Kids, 2 Tonnes, Biodiversity Collage, Digital Collage",
+    host: "National Volunteering and Philanthropy Centre",
+    url: "https://www.eventbrite.sg/e/climate-marathon-to-celebrate-cop28-tickets-750258010907?aff=oddtdtcreator",
+  }
+{
+    id: 5,
+    date: "28 November 2023 - Tuesday evening",
+    sessionType: "COP28 Climate Fresk with Panel",
+    host: "Singapore Sustainability Academy - CDL",
+    url: "https://association.climatefresk.org/training_sessions/b0d06fbe-84fb-4a17-8f1b-12eb2a5a557b/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
+  }
+{
+    id: 6,
+    date: "04 December 2023 - Monday evening",
+    sessionType: "Climate Fresk",
+    host: "Babilou Family - Alexandra Road",
+    url: "https://association.climatefresk.org/training_sessions/bd3879fb-138f-48d9-a56e-aebb7dc7517f/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
+  }
+{
+    id: 7,
+    date: "05 December 2023 - Tuesday evening",
+    sessionType: "Biodiversity Collage",
+    host: "L'Oréal offices - Raffles Quay",
+    url: "https://www.eventbrite.com/e/biodiversity-collage-loreal-singapore-tickets-658117395847",
+  }
+{
+    id: 8,
+    date: "05 December & 08 December 2023",
+    sessionType: "COP28 Climate Marathon - Climate Fresk & Biodiversity Collage",
+    host: "NUS Campus",
+    url: "https://www.eventbrite.sg/e/climate-awareness-workshops-to-celebrate-cop28-tickets-755279530407?aff=oddtdtcreator",
+  }
+{
+    id: 9,
+    date: "11 December 2023 - Monday evening",
+    sessionType: "Digital Collage",
+    host: "Viseo offices - Cecil St",
+    url: "https://www.eventbrite.sg/e/the-digital-collage-workshop-singapore-tickets-396945513717?aff=oddtdtcreator",
+  }
+{
+    id: 10,
+    date: "18 December 2023 - Monday evening",
+    sessionType: "Climate Fresk",
+    host: "Online",
+    url: "https://association.climatefresk.org/training_sessions/e5035044-bc82-4c1f-a882-9743dbb90ec2/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
   }
 ];
 
@@ -40,25 +75,32 @@ const participantSessionList: Session[] = [
 
 const instructorSessionList: Session[] = [
   {
-    id: 29,
-    date: "06 November 2023 (Monday evening)",
-    sessionType: "Digital Collage Training",
-    host: "Zenika Offices - BS Bendemeer Centre 3/F",
-    url: "https://www.billetweb.fr/training-asia-pacific-digital-collage",
-  },
-  {
     id: 32,
-    date: "13 November 2023 (Monday evening)",
+    date: "13 November 2023 - Monday evening",
     sessionType: "Climate Fresk Training",
     host: "Genius Central - Telok Ayer",
     url: "https://association.climatefresk.org/training_sessions/6b19a5af-2f34-4ebe-81b3-2413ec1613c8/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
   },
    {
     id: 33,
-    date: "15 November 2023 (Monday evening)",
+    date: "15 November 2023 - Wednesday evening",
     sessionType: "Digital Collage Training",
     host: "Online",
     url: "https://www.billetweb.fr/training-asia-pacific-digital-collage",
+  },
+    {
+    id: 32,
+    date: "29 November 2023 - Wednesday evening",
+    sessionType: "Digital Collage Training",
+    host: "Zenika offices - Bendemeer",
+    url: "https://www.billetweb.fr/training-asia-pacific-digital-collage",
+  },
+    {
+    id: 32,
+    date: "11 December 2023 - Monday evening",
+    sessionType: "Climate Fresk Training",
+    host: "Genius Central - Telok Ayer",
+    url: "https://association.climatefresk.org/training_sessions/6b19a5af-2f34-4ebe-81b3-2413ec1613c8/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
   },
 ]
 
