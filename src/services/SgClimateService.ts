db8e669522d89438b8261723d3d9c41890ae8cbe
Michael Isvy <michael.isvy@gmail.com>
2024-04-20 10:02:19 +0800

    Thibaut changes

--- src/services/SgClimateService.ts
+++ src/services/SgClimateService.ts
@@ -1,137 +1,221 @@
 const participantSessionList: Session[] = [
-   {
-    id: 1,
-    date: "3rd April 2024 - Wednesday evening",
-    sessionType: "Circular Economy",
-    host: "Chemistry",
-    url: "https://www.lafresquedeleconomiecirculaire.com/event-details/singapore",
-  },
   {
-    id: 7,
-    date: "06 April 2024 - Saturday morning",
-    sessionType: "Climate Fresk",
-    host: "Ismaili Civic",
-    url: "https://www.eventbrite.sg/e/climate-fresk-2tonnes-digital-collage-workshops-at-civic-singapore-tickets-849861036237",
-    },
-    {
-    id: 8,
-    date: "08 April 2024 - Monday evening",
-    sessionType: "Climate Fresk",
-    host: "Babilou Family - Alexandra Road",
-    url: "https://association.climatefresk.org/training_sessions/1d2225ea-07d3-4c03-bf3a-53ca3154c79d/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
-  },
+   id: 1,
+   date: "24 April 2024 - Wednesday evening",
+   sessionType: "Climate Marathon - all workshops available",
+   host: "Singapore Sustainability Academy - City Square Mall",
+   url: "https://www.eventbrite.sg/e/climate-marathon-to-celebrate-earth-week-tickets-877198322797?aff=oddtdtcreator",
+ },
   {
-    id: 9,
-    date: "06 April 2024 - Saturday morning",
-    sessionType: "2tonnes",
-    host: "Ismaili Civic",
-    url: "https://www.eventbrite.sg/e/climate-fresk-2tonnes-digital-collage-workshops-at-civic-singapore-tickets-849861036237",
-    },
+   id: 2,
+   date: "26 April 2024 - Wednesday evening",
+   sessionType: "Climate Fresk",
+   host: "Crane - OUE Downtown Gallery",
+   url: "https://association.climatefresk.org/training_sessions/8e0024c7-1b78-417c-a7e3-58502a9b5d3f/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
+   },
   {
-    id: 10,
-    date: "09 April 2024 - Tuesday evening",
-    sessionType: "Biodiversity Collage",
-    host: "Crane",
-    url: "https://www.eventbrite.com/e/biodiversity-collage-crane-tickets-819928978647",
-  },
-   {
-    id: 12,
-    date: "16 April 2024 - Tuesday evening",
-    sessionType: "Digital Collage",
-    host: "Online",
-    url: "https://www.billetweb.fr/asia-pacific-digital-collage",
-  },
-   {
-    id: 12,
-    date: "24 April 2024 - Wednesday evening",
-    sessionType: "Biodiversity Collage",
-    host: "Synthesis Creative data house",
-    url: "https://www.eventbrite.com/e/biodiversity-collage-synthesis-tickets-819690445187",
-    },
-   {
-    id: 13,
-    date: "30 April 2024 - Tuesday evening",
-    sessionType: "Digital Collage",
-    host: "Viseo - Cecil Street",
-    url: "https://www.billetweb.fr/asia-pacific-digital-collage",
-  },
+   id: 3,
+   date: "30 April 2024 - Tuesday evening",
+   sessionType: "Digital Collage",
+   host: "Viseo offices - Cecil Street",
+   url: "https://www.billetweb.fr/asia-pacific-digital-collage",
+   },
+  {
+   id: 4,
+   date: "1 May 2024 - Wednesday afternoon",
+   sessionType: "Circular Economy Collage",
+   host: "Chemistry - Bugis",
+   url: "https://www.eventbrite.sg/e/circular-economy-collage-chemistry-office-tickets-882499538877",
+   },
+  {
+   id: 5,
+   date: "6 May 2024 - Monday evening",
+   sessionType: "Climate Fresk",
+   host: "Babilou offices - Alexandra Road",
+   url: "https://association.climatefresk.org/training_sessions/a75fdd27-3e98-43bc-9cd3-8dcf602bf755/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
+   },
+  {
+   id: 6,
+   date: "6 May 2024 - Monday evening",
+   sessionType: "Climate Change Adaptation Workshop",
+   host: "The Hive - Carpenter Street",
+   url: "https://www.eventbrite.sg/e/adacc-adaptation-to-climate-change-workshops-tickets-862589377047",
+   },
+  {
+   id: 7,
+   date: "8 May 2024 - Wednesday evening",
+   sessionType: "Biodiversity Collage",
+   host: "Crane - OUE Downtown Gallery",
+   url: "https://www.eventbrite.com/e/biodiversity-collage-crane-tickets-819928978647",
+   },
+ {
+   id: 8,
+   date: "11 May 2024 - Saturday morning",
+   sessionType: "Climate Marathon - all workshops available",
+   host: "ISMAILI CIVIC - Geylang",
+   url: "https://www.eventbrite.com/e/climate-marathon-workshop-tickets-884769628777",
+ },
+  {
+   id: 9,
+   date: "13 May 2024 - Monday evening",
+   sessionType: "Climate Change Adaptation Workshop",
+   host: "Online",
+   url: "https://www.eventbrite.sg/e/adacc-adaptation-to-climate-change-workshops-online-tickets-862593790247",
+   },
+  {
+   id: 10,
+   date: "18 May 2024 - Saturday afternoon",
+   sessionType: "Biodiversity Collage",
+   host: "Palo IT - Beach road",
+   url: "https://www.eventbrite.sg/e/biodiversity-collage-palo-it-tickets-856910752127",
+   },
+  {
+   id: 11,
+   date: "18 May 2024 - Saturday afternoon",
+   sessionType: "Climate Fresk",
+   host: "Palo IT - Beach road",
+   url: "https://association.climatefresk.org/training_sessions/0f34a1b9-71b3-44ed-b0c5-8597742f55a1/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
+   },
+ {
+   id: 12,
+   date: "28 May 2024 - Tuesday evening",
+   sessionType: "Biodiversity Collage",
+   host: "Singapore Sustainability Academy - City Square Mall",
+   url: "https://www.eventbrite.sg/e/lets-celebrate-world-biodiversity-day-biodiversity-collage-tickets-886527215767",
+ },
+  {
+   id: 13,
+   date: "28 May 2024 - Tuesday evening",
+   sessionType: "Digital Collage",
+   host: "Viseo offices - Cecil Street",
+   url: "https://www.billetweb.fr/asia-pacific-digital-collage",
+   },
+  {
+   id: 14,
+   date: "3 June 2024 - Monday evening",
+   sessionType: "Climate Change Adaptation Workshop",
+   host: "The Hive - Carpenter Street",
+   url: "https://www.eventbrite.sg/e/adacc-adaptation-to-climate-change-workshops-tickets-862589377047",
+      },
+  {
+   id: 15,
+   date: "3 June 2024 - Monday evening",
+   sessionType: "Climate Fresk",
+   host: "Babilou offices - Alexandra Road",
+   url: "https://association.climatefresk.org/training_sessions/5aa66972-8125-4b6d-bca6-bfabfe2c8fcd/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
+   },
+  {
+   id: 16,
+   date: "4 June 2024 - Tuesday evening",
+   sessionType: "Biodiversity Collage",
+   host: "Crane - OUE Downtown Gallery",
+   url: "https://www.eventbrite.com/e/biodiversity-collage-crane-tickets-819928978647",
+   },
+  {
+   id: 17,
+   date: "10 June 2024 - Monday evening",
+   sessionType: "Climate Change Adaptation Workshop",
+   host: "Online",
+   url: "https://www.eventbrite.sg/e/adacc-adaptation-to-climate-change-workshops-online-tickets-862593790247",
+   },
+  {
+   id: 18,
+   date: "25 June 2024 - Tuesday evening",
+   sessionType: "Digital Collage",
+   host: "Viseo offices - Cecil Street",
+   url: "https://www.billetweb.fr/asia-pacific-digital-collage",
+   },
+  {
+   id: 19,
+   date: "8 July 2024 - Monday evening",
+   sessionType: "Climate Change Adaptation Workshop",
+   host: "The Hive - Carpenter Street",
+   url: "https://www.eventbrite.sg/e/adacc-adaptation-to-climate-change-workshops-tickets-862589377047",
+   },
+  {
+   id: 20,
+   date: "15 July 2024 - Monday evening",
+   sessionType: "Climate Change Adaptation Workshop",
+   host: "Online",
+   url: "https://www.eventbrite.sg/e/adacc-adaptation-to-climate-change-workshops-online-tickets-862593790247",
+   },
 ];
 
 const instructorSessionList: Session[] = [
+ {
+   id: 31,
+   date: "30 April 2024 - Tuesday evening",
+   sessionType: "Climate Fresk Training",
+   host: "Online",
+   url: "https://association.climatefresk.org/training_sessions/8c21c75f-1ee5-42f1-ad5e-7dfea1ea87b2/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
+ },
+   {
+   id: 32,
+   date: "9 May 2024 - Thursday evening",
+   sessionType: "Climate Fresk Training",
+   host: "Online",
+   url: "https://association.climatefresk.org/training_sessions/56bffc8e-9803-4e68-8bdc-c499977a6283/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
+ },
   {
-    id: 31,
-    date: "11 March 2024 - Monday evening",
-    sessionType: "Climate Fresk Training",
-    host: "Genius Central - Telok Ayer",
-    url: "https://association.climatefresk.org/training_sessions/d39272dc-ca22-4a41-a889-d76b6da4e6cc/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
-  },
-   {
-    id: 32,
-    date: "25 March 2024 - Monday evening",
-    sessionType: "Climate Fresk Training",
-    host: "Online",
-    url: "https://association.climatefresk.org/training_sessions/1f81fe62-dede-48fd-bd3c-7fb106f8bfb8/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
-  },
+   id: 33,
+   date: "13 May 2024 - Monday evening",
+   sessionType: "Climate Fresk Training",
+   host: "Genius Central - Telok Ayer",
+   url: "https://association.climatefresk.org/training_sessions/d339a316-1dee-4030-8677-4401601ecde8/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
+ },
   {
-    id: 33,
-    date: "26 March 2024 - Tuesday evening",
-    sessionType: "2tonnes Training",
-    host: "WeWork - Tanjong Pagar",
-    url: "https://www.eventbrite.sg/e/2tonnes-world-version-training-in-singapore-in-english-tickets-799715860667",
-  },
-   {
-    id: 34,
-    date: "01 April 2024 - Monday evening",
-    sessionType: "Digital Collage Training",
-    host: "Zenika offices - BS Center",
-    url: "https://www.billetweb.fr/training-asia-pacific-digital-collage",
-  },   
-   {
-    id: 35,
-    date: "11 April 2024 - Thursday evening",
-    sessionType: "Climate Fresk Training",
-    host: "Online",
-    url: "https://association.climatefresk.org/training_sessions/66389a24-b1e8-4f4a-a39d-760e483b5e5b/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
-   },
-   {
-    id: 36,
-    date: "15 April 2024 - Monday evening",
-    sessionType: "Climate Fresk Training",
-    host: "Genius Central - Telok Ayer",
-    url: "https://association.climatefresk.org/training_sessions/2678b794-3297-43ab-b486-43e168495831/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
-  },
+   id: 34,
+   date: "24 May 2024 - Friday evening",
+   sessionType: "Digital Collage Training",
+   host: "Online",
+   url: "https://www.billetweb.fr/training-asia-pacific-digital-collage",
+ },
+  {
+   id: 35,
+   date: "27 May 2024 - Monday evening",
+   sessionType: "Digital Collage Training",
+   host: "Zenika offices - Bendemeer",
+   url: "https://www.billetweb.fr/training-asia-pacific-digital-collage",
+ },
+  {
+   id: 36,
+   date: "10 June 2024 - Monday evening",
+   sessionType: "Climate Fresk Training",
+   host: "Genius Central - Telok Ayer",
+   url: "https://association.climatefresk.org/training_sessions/19e62329-adcb-4769-8320-16c4f5482345/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
+ },
 ]
 
 export function getParticipantSessionList() {
-  return participantSessionList;
+ return participantSessionList;
 }
 
 export function getInstructorSessionList() {
-  return instructorSessionList;
+ return instructorSessionList;
 }
 
 export function getParticipantSession(sessionId: number) {
-  return participantSessionList.find((item) => item.id === sessionId);
+ return participantSessionList.find((item) => item.id === sessionId);
 }
 
 export function getSessionList(type: string) {
-  const str2: string = "participant";
-  console.log(type);
-  console.log(str2);
-  console.log(str2.toString() == "participant");
-  if (type.toString() === "participant") {
-    return getParticipantSessionList();
-  } else if (type.toString() === "instructor") {
-    return getInstructorSessionList();
-  } else {
-    throw new Error("unknown type");
-  }
+ const str2: string = "participant";
+ console.log(type);
+ console.log(str2);
+ console.log(str2.toString() == "participant");
+ if (type.toString() === "participant") {
+   return getParticipantSessionList();
+ } else if (type.toString() === "instructor") {
+   return getInstructorSessionList();
+ } else {
+   throw new Error("unknown type");
+ }
 }
 
 export interface Session {
-  id: number;
-  date: string;
-  sessionType: string;
-  host: string;
-  url: string;
+ id: number;
+ date: string;
+ sessionType: string;
+ host: string;
+ url: string;
 }
