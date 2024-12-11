e2fd8eef029140b8a5b254d4404aa8dd8823439c
ThibautMg <139113604+ThibautMg@users.noreply.github.com>
2024-12-11 02:10:00 +0100

    Update SgClimateService.ts

--- src/services/SgClimateService.ts
+++ src/services/SgClimateService.ts
@@ -1,41 +1,6 @@
 const participantSessionList: Session[] = [
   {
    id: 1,
-   date: "03 December 2024 - Tuesday evening",
-   sessionType: "Digital Collage",
-   host: "Viseo offices - Cecil Street",
-   url: "https://www.billetweb.fr/asia-pacific-digital-collage",
-   },
-  {
-   id: 2,
-   date: "04 December 2024 - Wednesday evening",
-   sessionType: "2 Tonnes",
-   host: "Chemistry offices - Jalan Pinang",
-   url: "https://www.eventbrite.sg/e/2tonnes-workshop-chemistry-singapore-tickets-936348151527",
-   },
-  {
-   id: 3,
-   date: "04 December 2024 - Wednesday evening",
-   sessionType: "Circular Economy Collage",
-   host: "CEVA Logistics offices - Havelock Road",
-   url: "https://www.eventbrite.sg/e/circular-economy-collage-singapore-tickets-1052323426697?aff=erelexpmlt",
-   },
-  {
-   id: 4,
-   date: "05 December 2024 - Thursday evening",
-   sessionType: "Mobility Collage",
-   host: "The Hive - Carpenter Street",
-   url: "https://www.eventbrite.com/e/mobility-collage-public-workshop-singapore-tickets-991488407517",
-   },
-  {
-   id: 5,
-   date: "09 December 2024 - Monday evening",
-   sessionType: "Climate Change Adaptation Workshop",
-   host: "The Hive - Carpenter Street",
-   url: "https://www.eventbrite.sg/e/adacc-adaptation-to-climate-change-workshops-tickets-862589377047?aff=oddtdtcreator",
-   },
-   {
-   id: 6,
    date: "17 December 2024 - Tuesday evening",
    sessionType: "Climate Change Adaptation Workshop",
    host: "Online",
@@ -45,11 +10,39 @@ const participantSessionList: Session[] = [
 
 const instructorSessionList: Session[] = [
   {  
+   id: 32,
+   date: "09 January 2024 - Thursday evening",
+   sessionType: "Biodiversity Collage Training",
+   host: "Online",
+   url: "https://www.billetweb.fr/biodiversity-collage-training-online",
+   },
+  {  
+   id: 33,
+   date: "13 March 2025 - Thursday evening",
+   sessionType: "Biodiversity Collage Training",
+   host: "The Hive Carpenter - L2",
+   url: "https://www.eventbrite.sg/e/biodiversity-collage-training-to-become-facilitator-tickets-1110924377719?aff=oddtdtcreator",
+  },
+  {  
    id: 34,
-   date: "09 December 2024 - Monday evening",
-   sessionType: "Climate Fresk Training",
-   host: "Genius Central - Telok Ayer",
-   url: "https://association.climatefresk.org/training_sessions/5dbee7ca-af1e-4c82-8bb6-a6495b226503/show_public?language=en&tenant_token=36bd2274d3982262c0021755",
+   date: "17 April 2025 - Thursday evening",
+   sessionType: "Biodiversity Collage Training",
+   host: "Online",
+   url: "https://www.billetweb.fr/biodiversity-collage-training-online",
+  },
+{  
+   id: 35,
+   date: "07 May 2025 - Wednesday evening",
+   sessionType: "Biodiversity Collage Training",
+   host: "The Hive Carpenter - L2",
+   url: "https://www.eventbrite.sg/e/biodiversity-collage-training-to-become-facilitator-tickets-1110924377719?aff=oddtdtcreator",
+  },
+  {  
+   id: 36,
+   date: "17 June 2025 - Tuesday evening",
+   sessionType: "Biodiversity Collage Training",
+   host: "Online",
+   url: "https://www.billetweb.fr/biodiversity-collage-training-online",
   }
 ]
 
